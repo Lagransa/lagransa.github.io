8b33332672e1ceb679da22856b2a5303147e5085
Lagransa <574459243@qq.com>
2025-08-14 19:42:55 +0800

    调参

--- bulk_rna_seq_ml/ML_script/Bulk_RNA-seq_based_on_RandomForest.py
+++ bulk_rna_seq_ml/ML_script/Bulk_RNA-seq_based_on_RandomForest.py
@@ -124,8 +124,8 @@ class RF():
         self.X_test = X_test
         self.y_test = y_test
         self.param_grid = {
-            'n_estimators': [2, 5, 10, 20, 30, 50],
-            'max_depth':[None, 1, 2, 5, 10, 15],
+            'n_estimators': [10, 20, 30, 50, 100],
+            'max_depth':[1, 2, 5, 10, 15],
             'max_features':['sqrt', 0.1, 0.2, 0.5],
             'min_samples_leaf': [2, 3, 4, 5],
             'min_samples_split': [2, 3, 4]
