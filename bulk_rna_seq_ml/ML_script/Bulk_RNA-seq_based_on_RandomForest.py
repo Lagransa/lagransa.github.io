9a0a066b0f1341e76e0584e0d0b563fdb98e3c66
Lagransa <574459243@qq.com>
2025-08-14 22:24:33 +0800

    调参

--- bulk_rna_seq_ml/ML_script/Bulk_RNA-seq_based_on_RandomForest.py
+++ bulk_rna_seq_ml/ML_script/Bulk_RNA-seq_based_on_RandomForest.py
@@ -109,7 +109,7 @@ y.loc[y <= 6] = 0
 y.loc[(y >= 9)&(y <= 15)] = 1
 y.loc[(y > 15)] = 2
 ###############调整完毕#############################
-mask = np.argsort(-X_df.values.var(axis=0)).astype(int)[:1000]
+mask = np.argsort(-X_df.values.var(axis=0)).astype(int)[:5000]
 vst_df_o = vst_df.reset_index().iloc[:, 1:]
 vst_df_vared = vst_df_o.iloc[:, mask]
 vst_df_vared['Acta1'].values[:10]
